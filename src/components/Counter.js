8b4f526c60d596f015a63d6d084d0e3a8cdb3492
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-06-21 17:10:33 +0530

    add counter functionality

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,49 @@
-import { useState } from "react"
+import { useState , useEffect } from "react"
 import React from "react"
 import "./Counter.css"
 
 const Counter = () => {
     const [count, setCount] = useState(0)
+    const [isRunning, setIsRunning] = useState(false);
     
-    const addCount = () => {
-        setCount(prevCount => prevCount + 1)
-    }
+    useEffect(() => {
+        let interval;
 
-    const lessCount = () => {
-        setCount(prevCount => prevCount - 1)
-    }
+        if (isRunning) {
+            interval = setInterval(() => {
+                setCount(prevTime => prevTime + 1);
+            },1000)
+        } else if (!isRunning && count !== 0) {
+            clearInterval(interval);
+        }
+
+        return () => clearInterval(interval)
+    }, [isRunning])
+    
+    const startStopwatch = () => {
+        setIsRunning(true);
+    };
+
+    const stopStopwatch = () => {
+        setIsRunning(false);
+    };
+
+    const resetStopwatch = () => {
+        setIsRunning(false);
+        setCount(0);
+    };
+
 
     return (
         <div className="counter-area">
             <div className="container">
                 <h1>Stopwatch</h1>
-                <button className="less-btn" onClick={()=>lessCount()}>-</button>
                 <span>{count}</span>
-                <button className="add-btn" onClick={() => addCount()}>+</button>
+                <div className="btns">
+                    <button onClick={startStopwatch} className="btn">Start</button>
+                <button onClick={stopStopwatch} className="btn">Stop</button>
+                <button onClick={resetStopwatch} className="btn">Reset</button>
+                </div>
             </div>
         </div>
     )
